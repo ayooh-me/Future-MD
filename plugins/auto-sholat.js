4970a7a0d4ca2085425a961b943ff25d9b992e5c
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-03-16 05:46:40 +0800

    Add files via upload

--- plugins/auto-sholat.js
+++ plugins/auto-sholat.js
@@ -1,32 +1,67 @@
-export async function all(m) {
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let yyyy = today.getFullYear();
-let todays = yyyy + '/' + mm + '/' + dd;
-let data = await (await fetch("https://api.myquran.com/v1/sholat/jadwal/2622/" + todays)).json()
-let waktuSholat = data.data.jadwal;
 
-  let today = new Date();
-  let hour = today.getHours();
-  let minute = today.getMinutes();
-  let waktuSekarang = hour + ":" + minute
-  // Membandingkan waktu sekarang dengan waktu sholat
-  if (waktuSekarang == waktuSholat.imsak) {
-    m.reply("Sudah waktunya Imsak!");
-  } else if (waktuSekarang == waktuSholat.subuh) {
-    m.reply("Sudah waktunya sholat Subuh!");
-  } else if (waktuSekarang == waktuSholat.terbit) {
-    m.reply("Sudah waktunya Terbit!");
-  } else if (waktuSekarang == waktuSholat.dhuha) {
-    m.reply("Sudah waktunya sholat Dhuha!");
-  } else if (waktuSekarang == waktuSholat.dzuhur) {
-    m.reply("Sudah waktunya sholat Dzuhur!");
-  } else if (waktuSekarang == waktuSholat.ashar) {
-    m.reply("Sudah waktunya sholat Ashar!");
-  } else if (waktuSekarang == waktuSholat.maghrib) {
-    m.reply("Sudah waktunya sholat Maghrib!");
-  } else if (waktuSekarang == waktuSholat.isya) {
-    m.reply("Sudah waktunya sholat Isya!");
-  }
- }
+export async function all(m) {
+
+    /* Waktu Sholat Auto 
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    let todays = yyyy + '/' + mm + '/' + dd;
+    let data = await(await fetch("https://api.myquran.com/v1/sholat/jadwal/2622/" + todays)).json();
+    let waktuSholat = data.data.jadwal;
+    */
+    
+    /* Waktu Sholat Manual */
+     
+    let waktuSholat = {
+  imsak: '04:43',
+  subuh: '04:53',
+  terbit: '06:04',
+  dhuha: '06:31',
+  dzuhur: '12:15',
+  ashar: '15:19',
+  maghrib: '18:19',
+  isya: '19:27'
+  };
+    
+    /* Waktu Sekarang */
+    let date = new Date((new Date).toLocaleString("en-US", {
+        timeZone: "Asia/Makassar"
+    }));
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let formatHours = hours < 10 ? '0' + hours : hours;
+    let formatMinutes = minutes < 10 ? '0' + minutes : minutes;
+    let waktuSekarang = formatHours + ':' + formatMinutes;
+    
+    /* Opsi */
+    let logo_ = flaaa.getRandom();
+    let tag = '@' + m.sender.replace(/@.+/, '');
+    let caption = 'Hai kak ' + tag + ', ';
+    let sudah;
+
+    if (waktuSekarang == waktuSholat.imsak) {
+        sudah = "Daerah *Makassar* sudah waktunya *Imsak!*";
+    } else if (waktuSekarang == waktuSholat.subuh) {
+        sudah = "Daerah *Makassar* sudah waktunya *sholat Subuh!*";
+    } else if (waktuSekarang == waktuSholat.terbit) {
+        sudah = "Daerah *Makassar* sudah waktunya *Terbit!*";
+    } else if (waktuSekarang == waktuSholat.dhuha) {
+        sudah = "Daerah *Makassar* sudah waktunya *sholat Dhuha!*";
+    } else if (waktuSekarang == waktuSholat.dzuhur) {
+        sudah = "Daerah *Makassar* sudah waktunya *sholat Dzuhur!*";
+    } else if (waktuSekarang == waktuSholat.ashar) {
+        sudah = "Daerah *Makassar* sudah waktunya *sholat Ashar!*";
+    } else if (waktuSekarang == waktuSholat.maghrib) {
+        sudah = "Daerah *Makassar* sudah waktunya *sholat Maghrib!*";
+    } else if (waktuSekarang == waktuSholat.isya) {
+        sudah = "Daerah *Makassar* sudah waktunya *sholat Isya!*";
+    }
+    
+    if (sudah) {
+        return this.sendFile(m.chat, logo_ + sudah.split(" ").pop().replace(/\*/g, ""), 'out.png', caption + sudah, m, null, {
+            mentions: [m.sender]
+        });
+    }
+    
+}
