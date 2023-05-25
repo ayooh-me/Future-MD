aadf4ee23d9a77189168c443b73cea73097f7764
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-05-25 22:17:45 +0800

    Add files via upload

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -3,83 +3,105 @@ import fetch from "node-fetch"
 import uploadImage from "../lib/uploadImage.js"
 
 let handler = async (m, {
-    conn,
+    command,
     usedPrefix,
-    args,
-    command
+    conn,
+    text,
+    args
 }) => {
-    let query = "input text\nEx. .ss s.id\n<command> <tex>"
-    let text
-    if (args.length >= 1) {
-        text = args.slice(0).join(" ")
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw query
-    let urut = text.split`|`
-    let one = urut[0]
-    let two = urut[1]
-    let three = urut[2]
-    let LinkReg = /https?:\/\//.test(two) ? two : "https://" + two
 
-    if (["ss", "ssf", "ssweb"].includes(command)) {
-        m.reply(wait)
-        let lis = [
-            "shot.screenshotapi.net",
-            "api.popcat.xyz",
-            "api.apiflash.com",
-            "image.thum.io",
-            "www.screenshotmachine.com",
-            "www.urlbox.io"
-        ]
-        let row = Object.keys(lis).map((v, index) => ({
-            title: "By " + lis[v],
-            description: "Bot " + author,
-            rowId: usedPrefix + "ssget " + lis[v] + "|" + text
-        }))
-        let button = {
-            buttonText: `☂️ Tema Disini ☂️`,
-            description: `⚡ Silakan pilih tema di tombol di bawah...\n*Teks yang anda kirim:* ${text}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`,
-            footerText: wm
+    let lister = [
+        "v1",
+        "v2",
+        "v3",
+        "v4",
+        "v5",
+        "v6"
+
+    ]
+let spas = "                "
+    let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
+    if (!lister.includes(feature.toLowerCase())) return m.reply("*Example:*\n" + usedPrefix + command + " v2 link\n\n*Pilih type yg ada*\n" + lister.map((v, index) => "  ○ " + v.toUpperCase()).join("\n"))
+    
+    if (lister.includes(feature)) {
+        if (feature == "v1") {
+            let LinkReg = /https?:\/\//.test(inputs) ? inputs : "https://" + inputs
+            if (!LinkReg) return m.reply("Input query link")
+            m.reply(wait)
+                try {
+                let res = "https://shot.screenshotapi.net/screenshot?token=WCCYKR0-X5CMMV0-JB4G5Z5-P6SPC8R&url=" + LinkReg + "&full_page=true&fresh=true&output=image&file_type=jpg"
+                await conn.sendFile(m.chat, res, "", "*Request:* " + m.name, m)
+            } catch (e) {
+                throw eror
+            }
         }
-        return conn.sendListM(m.chat, button, row, m)
-    }
-    if (command == "ssget") {
-        try {
-            if (one == "www.screenshotmachine.com") {
+        if (feature == "v2") {
+            let LinkReg = /https?:\/\//.test(inputs) ? inputs : "https://" + inputs
+            if (!LinkReg) return m.reply("Input query link")
+            m.reply(wait)
+                try {
+                let res = "https://api.popcat.xyz/screenshot?url=" + LinkReg
+                await conn.sendFile(m.chat, res, "", "*Request:* " + m.name, m)
+            } catch (e) {
+                throw eror
+            }
+
+        }
+        if (feature == "v3") {
+            let LinkReg = /https?:\/\//.test(inputs) ? inputs : "https://" + inputs
+            if (!LinkReg) return m.reply("Input query link")
+            m.reply(wait)
+            try{
+                let res = "https://api.apiflash.com/v1/urltoimage?access_key=7eea5c14db5041ecb528f68062a7ab5d&wait_until=page_loaded&url=" + LinkReg
+                await conn.sendFile(m.chat, res, "", "*Request:* " + m.name, m)
+            } catch (e) {
+                throw eror
+            }
+
+        }
+        if (feature == "v4") {
+            let LinkReg = /https?:\/\//.test(inputs) ? inputs : "https://" + inputs
+            if (!LinkReg) return m.reply("Input query link")
+            m.reply(wait)
+                try {
+                let res = "https://image.thum.io/get/fullpage/" + LinkReg
+                await conn.sendFile(m.chat, res, "", "*Request:* " + m.name, m)
+            } catch (e) {
+                throw eror
+            }
+
+        }
+        if (feature == "v5") {
+            let LinkReg = /https?:\/\//.test(inputs) ? inputs : "https://" + inputs
+            if (!LinkReg) return m.reply("Input query link")
+            m.reply(wait)
+                try {
                 let st = await ssweb(LinkReg, "full", "desktop")
                 let res = await uploadImage(st)
-                await conn.sendFile(m.chat, res, "", author, m)
+                await conn.sendFile(m.chat, res, "", "*Request:* " + m.name, m)
+            } catch (e) {
+                throw eror
             }
-            if (one == "www.urlbox.io") {
-                let res = await ssweb2(LinkReg)
-                await conn.sendFile(m.chat, res, "", author, m)
-            }
-            if (one == "shot.screenshotapi.net") {
-                let res = "https://shot.screenshotapi.net/screenshot?token=WCCYKR0-X5CMMV0-JB4G5Z5-P6SPC8R&url=" + LinkReg + "&full_page=true&fresh=true&output=image&file_type=jpg"
-                await conn.sendFile(m.chat, res, "", author, m)
-            }
-            if (one == "api.popcat.xyz") {
-                let res = "https://api.popcat.xyz/screenshot?url=" + LinkReg
-                await conn.sendFile(m.chat, res, "", author, m)
-            }
-            if (one == "api.apiflash.com") {
-                let res = "https://api.apiflash.com/v1/urltoimage?access_key=7eea5c14db5041ecb528f68062a7ab5d&wait_until=page_loaded&url=" + LinkReg
-                await conn.sendFile(m.chat, res, "", author, m)
-            }
-            if (one == "image.thum.io") {
-                let res = "https://image.thum.io/get/fullpage/" + LinkReg
-                await conn.sendFile(m.chat, res, "", author, m)
-            }
-        } catch (e) {
-            throw eror
-        }
-    }
 
+        }
+        if (feature == "v6") {
+            let LinkReg = /https?:\/\//.test(inputs) ? inputs : "https://" + inputs
+            if (!LinkReg) return m.reply("Input query link")
+            m.reply(wait)
+                try {
+                let res = await ssweb2(LinkReg)
+                await conn.sendFile(m.chat, res, "", "*Request:* " + m.name, m)
+            } catch (e) {
+                throw eror
+            }
+
+        }
+
+    }
 }
 handler.help = ["ss", "ssf", "ssweb"]
 handler.tags = ["tools"]
-handler.command = /^ss(get|web|f)?$/i
-
+handler.command = /^ss(web|f)?$/i
 export default handler
 
  async function ssweb(url = "", full = false, type = "desktop") {
